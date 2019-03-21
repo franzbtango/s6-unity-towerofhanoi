0593833c5675576a3c97e7f2efd13f8aa450f5f1
franzbtango <franzbtango@gmail.com>
2019-03-21 20:06:56 +0800

    added transitions

--- Assets/Scripts/DiskScript.cs
+++ Assets/Scripts/DiskScript.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class DiskScript : MonoBehaviour
 {
@@ -6,19 +7,31 @@ public class DiskScript : MonoBehaviour
     private Vector2 position;
 
     public void LiftDisk(Rod origin) {
-        transform.position = new Vector2(transform.position.x, 3.5f);
+        //transform.position = new Vector2(transform.position.x, 3.5f);
+        StartCoroutine(LerpPosition(new Vector2(transform.position.x, 3.5f), 0.1f));
         origin.Disks.Remove(ID);
         FindObjectOfType<Globals>().Play("LiftDisk");
     }
 
     public void DropDisk(Rod destination, bool initial) {
         int diskCount = destination.Disks.Count;
-        position = new Vector2(destination.ID == 0 ? -5.25f : destination.ID == 1 ? 0 : 5.25f, -3 + 0.5f * diskCount);
-        transform.position = position;
+        //position = new Vector2(destination.ID == 0 ? -5.25f : destination.ID == 1 ? 0 : 5.25f, -3 + 0.5f * diskCount);
+        //transform.position = position;
+        StartCoroutine(LerpPosition(new Vector2(destination.ID == 0 ? -5.25f : destination.ID == 1 ? 0 : 5.25f, -3 + 0.5f * diskCount), 0.1f));
 
         if (!initial) {
             destination.AddDisk(ID, this.gameObject);
             FindObjectOfType<Globals>().Play("DropDisk");
         }
+    }
+
+    private IEnumerator LerpPosition (Vector2 destination, float time) {
+        float elapsedTime = 0 + Time.deltaTime;
+        while(elapsedTime < time) {
+            transform.position = Vector2.Lerp(transform.position, destination, elapsedTime / time);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        Debug.Log("test");
     }
 }
